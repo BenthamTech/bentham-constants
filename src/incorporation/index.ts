354e888f587631048d56153a6e8a6a5b00ea13d3
Sairam Koripelly <sairamk0@gmail.com>
2026-07-15 18:56:22 +0530

    added incorporation filing override

--- src/incorporation/index.ts
+++ src/incorporation/index.ts
@@ -1,3 +1,4 @@
 export { calculateStampDuty, calculateIncorporationCost, getAvailableStates, toDisplayName } from './calculator';
 export type { FeeItem, FeeBreakdown, AoaFormula, StateStampDutyEntry } from './types';
 export { default as pricingConfig } from './pricing-config.json';
+export { FilingOverrides } from './filing-overrides';
